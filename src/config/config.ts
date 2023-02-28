0a494893e36cebdcc30d3706fef50481b04f143e
ankitnag0 <heroankit0@gmail.com>
2023-02-28 18:19:54 +0530

    added port to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  nodeEnv: process.env.NODE_ENV,
+  nodeEnv: process.env.NODE_ENV || "production",
+  port: process.env.PORT || 3000,
   databaseURI:
     process.env.DATABASE_URI + (process.env.NODE_ENV === "test" ? "-test" : ""),
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret:
+    process.env.JWT_SECRET ||
+    "bdf5feb119974ea22c4495f613974c014b18b4811898c719b016ada1bced55a4676e67e6d44f65faa474dc9ac1a451a10a5cb73650d7f6294c1a2ed50faeb1dc",
   jwtExpiration: process.env.JWT_EXPIRATION,
   redisCacheExpiration: parseInt(process.env.REDIS_CACHE_EXPIRATION),
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import app from "./app";
+import config from "./config/config";
 import { connect } from "./database/db";
 import { redisConnect } from "./database/redisClient";
 
 (async () => {
   await connect();
   await redisConnect();
-  app.listen(3000, () => {
-    console.log(`Server listening on port 3000`);
+  app.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}`);
   });
 })();
