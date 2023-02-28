be428e93222ac9a75745045a8a05aaa6bd780fbb
ankitnag0 <heroankit0@gmail.com>
2023-02-28 15:15:06 +0530

    Added all tests for member routes

--- tests/routes/member.test.ts
+++ tests/routes/member.test.ts
@@ -12,7 +12,7 @@ describe("Member", () => {
     await disconnect();
   });
 
-  it("should register a user successfully", async () => {
+  it("should register a member successfully", async () => {
     const response = await request(app)
       .post("/api/members")
       .send({
@@ -23,5 +23,60 @@ describe("Member", () => {
         phoneNumber: "7845123658",
       })
       .expect(201);
+    const members = await Member.find();
+    expect(members.length).toEqual(1);
+    expect(members[0].email).toEqual("ankit@email.com");
+  });
+
+  it("should not create a member if it already exists", async () => {
+    const response = await request(app)
+      .post("/api/members")
+      .send({
+        username: "ankit",
+        email: "ankit@email.com",
+        password: "password",
+        address: "Address",
+        phoneNumber: "7845123658",
+      })
+      .expect(400);
+    expect(response.body).toEqual({
+      success: false,
+      message: "Username or email already exists",
+    });
+  });
+
+  it("should login a member successfully", async () => {
+    const response = await request(app)
+      .post("/api/members/login")
+      .send({
+        email: "ankit@email.com",
+        password: "password",
+      })
+      .expect(200);
+    expect(response.body.success).toEqual(true);
+  });
+
+  it("should return 401 if the member is not found", async () => {
+    const response = await request(app)
+      .post("/api/members/login")
+      .send({
+        email: "invalid@email.com",
+        password: "password",
+      })
+      .expect(401);
+    expect(response.body.success).toEqual(false);
+    expect(response.body.message).toEqual("Invalid email or password");
+  });
+
+  it("should return 401 if the password is invalid", async () => {
+    const response = await request(app)
+      .post("/api/members/login")
+      .send({
+        email: "ankit@email.com",
+        password: "wrongPassword",
+      })
+      .expect(401);
+    expect(response.body.success).toEqual(false);
+    expect(response.body.message).toEqual("Invalid email or password");
   });
 });
